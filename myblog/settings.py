ce55b0784db5757c42bd6aa9ee9666ef8387016f
Ting <kelanmomo@hotmail.com>
2019-01-02 16:25:39 +0800

    requirement file upload

--- myblog/settings.py
+++ myblog/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/1.11/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
-
+from decouple import config, Csv 
+import dj_database_url 
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -20,12 +21,13 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bdiw*54$l@-rl!aribvo=o0_so-pk7_%$_=+ajthn@o4v2!o=0'
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool) 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv()) 
 
 
 # Application definition
@@ -86,6 +88,12 @@ DATABASES = {
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = { 
+#     'default': dj_database_url.config( 
+#         default=config('DATABASE_URL') 
+#     )        
+# }
 
 
 # Password validation
